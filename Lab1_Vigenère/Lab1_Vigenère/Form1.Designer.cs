754af5440a85b839a0c95e6d11d7bef9b54c7ff3
longle <22520813@gm.uit.edu.vn>
2024-10-05 15:01:27 +0700

    Vigenère_cipher

---
+++ Lab1_Vigenère/Lab1_Vigenère/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Lab1_Vigenère
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tab_encrypt = new TabPage();
+            panel2 = new Panel();
+            tb_cyphertext_encrypt = new TextBox();
+            panel3 = new Panel();
+            label3 = new Label();
+            panel1 = new Panel();
+            btn_encrypt = new Button();
+            tb_plaintext_encrypt = new TextBox();
+            tb_key_encrypt = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            label4 = new Label();
+            label5 = new Label();
+            tb_key_decrypt = new TextBox();
+            tb_cyphertext_decrypt = new TextBox();
+            btn_decrypt = new Button();
+            panel4 = new Panel();
+            label6 = new Label();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            tb_plaintext_decrypt = new TextBox();
+            tabControl1.SuspendLayout();
+            tab_encrypt.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tab_encrypt);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 477);
+            tabControl1.TabIndex = 0;
+            // 
+            // tab_encrypt
+            // 
+            tab_encrypt.Controls.Add(panel2);
+            tab_encrypt.Controls.Add(panel1);
+            tab_encrypt.Location = new Point(4, 24);
+            tab_encrypt.Name = "tab_encrypt";
+            tab_encrypt.Padding = new Padding(3);
+            tab_encrypt.Size = new Size(792, 449);
+            tab_encrypt.TabIndex = 0;
+            tab_encrypt.Text = "Encrypt";
+            tab_encrypt.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(tb_cyphertext_encrypt);
+            panel2.Controls.Add(panel3);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(403, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(386, 443);
+            panel2.TabIndex = 5;
+            // 
+            // tb_cyphertext_encrypt
+            // 
+            tb_cyphertext_encrypt.BackColor = SystemColors.Window;
+            tb_cyphertext_encrypt.Dock = DockStyle.Fill;
+            tb_cyphertext_encrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_cyphertext_encrypt.ForeColor = SystemColors.WindowText;
+            tb_cyphertext_encrypt.Location = new Point(0, 30);
+            tb_cyphertext_encrypt.Multiline = true;
+            tb_cyphertext_encrypt.Name = "tb_cyphertext_encrypt";
+            tb_cyphertext_encrypt.ReadOnly = true;
+            tb_cyphertext_encrypt.ScrollBars = ScrollBars.Vertical;
+            tb_cyphertext_encrypt.Size = new Size(386, 413);
+            tb_cyphertext_encrypt.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label3);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(386, 30);
+            panel3.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(139, 5);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Cyphertext";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btn_encrypt);
+            panel1.Controls.Add(tb_plaintext_encrypt);
+            panel1.Controls.Add(tb_key_encrypt);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(400, 443);
+            panel1.TabIndex = 4;
+            // 
+            // btn_encrypt
+            // 
+            btn_encrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_encrypt.Location = new Point(32, 383);
+            btn_encrypt.Name = "btn_encrypt";
+            btn_encrypt.Size = new Size(109, 46);
+            btn_encrypt.TabIndex = 5;
+            btn_encrypt.Text = "Encrypt";
+            btn_encrypt.UseVisualStyleBackColor = true;
+            btn_encrypt.Click += btn_encrypt_Click_1;
+            // 
+            // tb_plaintext_encrypt
+            // 
+            tb_plaintext_encrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_plaintext_encrypt.Location = new Point(32, 148);
+            tb_plaintext_encrypt.Multiline = true;
+            tb_plaintext_encrypt.Name = "tb_plaintext_encrypt";
+            tb_plaintext_encrypt.ScrollBars = ScrollBars.Vertical;
+            tb_plaintext_encrypt.Size = new Size(346, 222);
+            tb_plaintext_encrypt.TabIndex = 3;
+            // 
+            // tb_key_encrypt
+            // 
+            tb_key_encrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_key_encrypt.Location = new Point(32, 55);
+            tb_key_encrypt.Multiline = true;
+            tb_key_encrypt.Name = "tb_key_encrypt";
+            tb_key_encrypt.ScrollBars = ScrollBars.Vertical;
+            tb_key_encrypt.Size = new Size(346, 41);
+            tb_key_encrypt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(32, 124);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Enter Plaintext";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(32, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Enter Key";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(panel6);
+            tabPage2.Controls.Add(panel4);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 449);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(32, 31);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 21);
+            label4.TabIndex = 0;
+            label4.Text = "Enter Key";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(32, 124);
+            label5.Name = "label5";
+            label5.Size = new Size(109, 21);
+            label5.TabIndex = 1;
+            label5.Text = "Enter Plaintext";
+            // 
+            // tb_key_decrypt
+            // 
+            tb_key_decrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_key_decrypt.Location = new Point(32, 55);
+            tb_key_decrypt.Multiline = true;
+            tb_key_decrypt.Name = "tb_key_decrypt";
+            tb_key_decrypt.ScrollBars = ScrollBars.Vertical;
+            tb_key_decrypt.Size = new Size(346, 41);
+            tb_key_decrypt.TabIndex = 2;
+            // 
+            // tb_cyphertext_decrypt
+            // 
+            tb_cyphertext_decrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_cyphertext_decrypt.Location = new Point(32, 148);
+            tb_cyphertext_decrypt.Multiline = true;
+            tb_cyphertext_decrypt.Name = "tb_cyphertext_decrypt";
+            tb_cyphertext_decrypt.ScrollBars = ScrollBars.Vertical;
+            tb_cyphertext_decrypt.Size = new Size(346, 222);
+            tb_cyphertext_decrypt.TabIndex = 3;
+            // 
+            // btn_decrypt
+            // 
+            btn_decrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_decrypt.Location = new Point(32, 383);
+            btn_decrypt.Name = "btn_decrypt";
+            btn_decrypt.Size = new Size(109, 46);
+            btn_decrypt.TabIndex = 5;
+            btn_decrypt.Text = "Decrypt";
+            btn_decrypt.UseVisualStyleBackColor = true;
+            btn_decrypt.Click += btn_decrypt_Click;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(btn_decrypt);
+            panel4.Controls.Add(tb_cyphertext_decrypt);
+            panel4.Controls.Add(tb_key_decrypt);
+            panel4.Controls.Add(label5);
+            panel4.Controls.Add(label4);
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(3, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(400, 443);
+            panel4.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(153, 5);
+            label6.Name = "label6";
+            label6.Size = new Size(69, 21);
+            label6.TabIndex = 2;
+            label6.Text = "Plaintext";
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(label6);
+            panel5.Dock = DockStyle.Top;
+            panel5.Location = new Point(0, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(386, 30);
+            panel5.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(tb_plaintext_decrypt);
+            panel6.Controls.Add(panel5);
+            panel6.Dock = DockStyle.Fill;
+            panel6.Location = new Point(403, 3);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(386, 443);
+            panel6.TabIndex = 8;
+            // 
+            // tb_plaintext_decrypt
+            // 
+            tb_plaintext_decrypt.Dock = DockStyle.Fill;
+            tb_plaintext_decrypt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_plaintext_decrypt.Location = new Point(0, 30);
+            tb_plaintext_decrypt.Multiline = true;
+            tb_plaintext_decrypt.Name = "tb_plaintext_decrypt";
+            tb_plaintext_decrypt.ScrollBars = ScrollBars.Vertical;
+            tb_plaintext_decrypt.Size = new Size(386, 413);
+            tb_plaintext_decrypt.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 477);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tab_encrypt.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tab_encrypt;
+        private TextBox tb_cyphertext_encrypt;
+        private TabPage tabPage2;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label3;
+        private Panel panel1;
+        private Button btn_encrypt;
+        private TextBox tb_plaintext_encrypt;
+        private TextBox tb_key_encrypt;
+        private Label label2;
+        private Label label1;
+        private Panel panel6;
+        private TextBox tb_plaintext_decrypt;
+        private Panel panel5;
+        private Label label6;
+        private Panel panel4;
+        private Button btn_decrypt;
+        private TextBox tb_cyphertext_decrypt;
+        private TextBox tb_key_decrypt;
+        private Label label5;
+        private Label label4;
+    }
+}
---
+++ Lab1_Vigenère/Lab1_Vigenère/Form1.cs
@@ -0,0 +1,199 @@
+namespace Lab1_Vigenère
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_encrypt_Click_1(object sender, EventArgs e)
+        {
+            tb_cyphertext_encrypt.Text = "";
+            if (string.IsNullOrEmpty(tb_key_encrypt.Text.Trim()) || string.IsNullOrEmpty(tb_plaintext_encrypt.Text.Trim()))
+            {
+                MessageBox.Show("Hãy nhập đầy đủ!!!");
+                return;
+            }
+            string Plaintext = tb_plaintext_encrypt.Text.Trim();
+            string Key = tb_key_encrypt.Text.Trim();
+
+            string Cyphertext = "";
+            int i = 0;
+            foreach (var item in Plaintext)
+            {
+                if (i == Key.Length)
+                {
+                    i = 0;
+                }
+                if (item == ' ')
+                {
+                    Cyphertext += item;
+                    continue;
+                }
+                int indexOfCi = (convertToInt(item) + convertToInt(Key[i])) % 26;
+                char Ci = convertToCharacter(indexOfCi);
+                i++;
+                Cyphertext += Ci;
+            }
+            tb_cyphertext_encrypt.Text = Cyphertext;
+        }
+        private int convertToInt(char ascii)
+        {
+            switch (ascii)
+            {
+                case 'A':
+                case 'a':
+                    return 0;
+                case 'B':
+                case 'b':
+                    return 1;
+                case 'C':
+                case 'c':
+                    return 2;
+                case 'D':
+                case 'd':
+                    return 3;
+                case 'E':
+                case 'e':
+                    return 4;
+                case 'F':
+                case 'f':
+                    return 5;
+                case 'G':
+                case 'g':
+                    return 6;
+                case 'H':
+                case 'h':
+                    return 7;
+                case 'I':
+                case 'i':
+                    return 8;
+                case 'J':
+                case 'j':
+                    return 9;
+                case 'K':
+                case 'k':
+                    return 10;
+                case 'L':
+                case 'l':
+                    return 11;
+                case 'M':
+                case 'm':
+                    return 12;
+                case 'N':
+                case 'n':
+                    return 13;
+                case 'O':
+                case 'o':
+                    return 14;
+                case 'P':
+                case 'p':
+                    return 15;
+                case 'Q':
+                case 'q':
+                    return 16;
+                case 'R':
+                case 'r':
+                    return 17;
+                case 'S':
+                case 's':
+                    return 18;
+                case 'T':
+                case 't':
+                    return 19;
+                case 'U':
+                case 'u':
+                    return 20;
+                case 'V':
+                case 'v':
+                    return 21;
+                case 'W':
+                case 'w':
+                    return 22;
+                case 'X':
+                case 'x':
+                    return 23;
+                case 'Y':
+                case 'y':
+                    return 24;
+                case 'Z':
+                case 'z':
+                    return 25;
+            }
+            return ' ';
+        }
+        private char convertToCharacter(int asciiValue)
+        {
+            switch (asciiValue)
+            {
+                case 0: return 'A';
+                case 1: return 'B';
+                case 2: return 'C';
+                case 3: return 'D';
+                case 4: return 'E';
+                case 5: return 'F';
+                case 6: return 'G';
+                case 7: return 'H';
+                case 8: return 'I';
+                case 9: return 'J';
+                case 10: return 'K';
+                case 11: return 'L';
+                case 12: return 'M';
+                case 13: return 'N';
+                case 14: return 'O';
+                case 15: return 'P';
+                case 16: return 'Q';
+                case 17: return 'R';
+                case 18: return 'S';
+                case 19: return 'T';
+                case 20: return 'U';
+                case 21: return 'V';
+                case 22: return 'W';
+                case 23: return 'X';
+                case 24: return 'Y';
+                case 25: return 'Z';
+            }
+            return ' ';
+        }
+
+        private void tb_key_encrypt_TextChanged(object sender, EventArgs e)
+        {
+            if (tb_key_encrypt.Text.Trim().Contains(" "))
+            {
+                MessageBox.Show("Key không thể chứa khoảng cách!!!");
+                return;
+            }
+        }
+
+        private void btn_decrypt_Click(object sender, EventArgs e)
+        {
+            tb_plaintext_decrypt.Text = "";
+            if (string.IsNullOrEmpty(tb_key_decrypt.Text.Trim()) || string.IsNullOrEmpty(tb_cyphertext_decrypt.Text.Trim()))
+            {
+                MessageBox.Show("Hãy nhập đầy đủ!!!");
+                return;
+            }
+            string Cyphertext = tb_cyphertext_decrypt.Text.Trim();
+            string Key = tb_key_decrypt.Text.Trim();
+            string Plaintext = "";
+            int i = 0;
+            foreach (var item in Cyphertext)
+            {
+                if (i == Key.Length)
+                {
+                    i = 0;
+                }
+                if (item == ' ')
+                {
+                    Plaintext += item;
+                    continue;
+                }
+                int indexOfCi = (convertToInt(item) - convertToInt(Key[i]) + 26) % 26;
+                char Ci = convertToCharacter(indexOfCi);
+                i++;
+                Plaintext += Ci;
+            }
+            tb_plaintext_decrypt.Text = Plaintext;
+        }
+    }
+}
