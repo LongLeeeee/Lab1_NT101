e1eef5059bed8c1092f1d70bc3fce4b56c5d1e61
Phát Lâm <22521068@gm.uit.edu.vn>
2024-10-06 13:24:32 +0700

    tp

---
+++ LAB1_CAU2/LAB1_CAU2/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace LAB1_CAU2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Plaintext = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.shiftnumber = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.to_value = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultshow = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Plaintext
+            // 
+            this.Plaintext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Plaintext.Location = new System.Drawing.Point(183, 35);
+            this.Plaintext.Multiline = true;
+            this.Plaintext.Name = "Plaintext";
+            this.Plaintext.Size = new System.Drawing.Size(718, 125);
+            this.Plaintext.TabIndex = 0;
+            this.Plaintext.TextChanged += new System.EventHandler(this.Plaintext_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Plaintext";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Value Number";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // shiftnumber
+            // 
+            this.shiftnumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.shiftnumber.Location = new System.Drawing.Point(158, 41);
+            this.shiftnumber.Multiline = true;
+            this.shiftnumber.Name = "shiftnumber";
+            this.shiftnumber.Size = new System.Drawing.Size(78, 37);
+            this.shiftnumber.TabIndex = 4;
+            this.shiftnumber.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.shiftnumber);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(37, 197);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(864, 133);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Key entry as";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.to_value);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(37, 345);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(864, 173);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Properties of the chosen encrytion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0";
+            // 
+            // to_value
+            // 
+            this.to_value.AutoSize = true;
+            this.to_value.Location = new System.Drawing.Point(80, 119);
+            this.to_value.Name = "to_value";
+            this.to_value.Size = new System.Drawing.Size(340, 24);
+            this.to_value.TabIndex = 0;
+            this.to_value.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            this.to_value.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(340, 24);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            this.label6.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 24);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "TO";
+            this.label7.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "From";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Shift of";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 58);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Decryt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultshow
+            // 
+            this.resultshow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultshow.Location = new System.Drawing.Point(78, 608);
+            this.resultshow.Multiline = true;
+            this.resultshow.Name = "resultshow";
+            this.resultshow.Size = new System.Drawing.Size(799, 153);
+            this.resultshow.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 783);
+            this.Controls.Add(this.resultshow);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Plaintext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Plaintext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox shiftnumber;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label to_value;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox resultshow;
+    }
+}
+
---
+++ LAB1_CAU2/LAB1_CAU2/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB1_CAU2
+{
+    public partial class Form1 : Form
+    {
+
+      
+        private char[] upperalphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        private char[] loweralphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        private char[,] vigenereTableUppercase = new char[26, 26];
+        private char[,] vigenereTableLowercase = new char[26, 26];
+
+
+        public Form1() 
+        {
+            InitializeComponent();
+            //taobanvigenereTable();
+        }
+        
+       /* private void taobanvigenereTable()
+        {
+
+            // i là số thứ tự của ký tự hàng ngang
+            //j là số thứ tụ của ký tự hàng dọc
+            // ví dụ i = 2 :B
+            //j = 3 
+            // => E là thứ tự thứ 5 của hàng ngang 
+            for(int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j<26;j++) 
+                {
+                    vigenereTable[i,j] = alpahbet[(i+j)%26];
+
+                }
+            }
+        }*/
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            /*string b = "";
+            string a = shiftnumber.Text;
+            int c = 0;
+            label4.Text = a;
+            if (a == "")
+            {
+                to_value.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+            else
+            {
+                c = int.Parse(a);
+                for (int i = 0; i < 26; i++)
+                {
+                    b += vigenereTable[c, i];
+
+                }
+                to_value.Text = b;
+            }*/
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string Decrytion( string ciphertext, int shift)
+        {
+
+            string plaint = "";
+            
+            foreach(char c in ciphertext)
+            {
+                if (char.IsUpper(c))
+                {
+                    /*int origianlIndex = Array.IndexOf(alpahbet, c);
+                    int origianlIndex = Array.IndexOf(alpahbet, c);
+                    plaint += vigenereTable[0, newindex];*/ 
+                    // cách dùng bản 
+
+
+                    int origianlIndex = Array.IndexOf(upperalphabet, c);
+
+                    int newIndex = (origianlIndex - shift+26) % 26;
+                    plaint += upperalphabet[newIndex];
+
+                    //int newIndex = (c - 'A' - shift + 26) % 26 + 'A'; //cach dùng mã ascii
+                    //plaint += (char)newIndex;
+
+
+
+
+
+                }
+                else if (char.IsLower(c))
+                {
+                    // int newIndex = (c - 'a' - shift + 26) % 26 + 'a';
+                    //plaint += (char)newIndex;
+                    int origianlIndex = Array.IndexOf(loweralphabet, c);
+                    int newIndex = (origianlIndex - shift+26) % 26;
+
+                    plaint += loweralphabet[newIndex];
+                }
+                else
+                {
+                    plaint += c; 
+                }
+            }
+            return plaint;
+            //key = 19
+        }
+        private string encrytion(string ciphertext, int shift)
+        {
+            string a = "";
+            return a;
+        }
+        private void Plaintext_TextChanged(object sender, EventArgs e)
+        {
+       
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int shift = int.Parse(shiftnumber.Text);
+            string ciphertext = Plaintext.Text;
+            string plaintext = Decrytion(ciphertext, shift);
+            resultshow.Text = plaintext;
+
+        }
+    }
+}
